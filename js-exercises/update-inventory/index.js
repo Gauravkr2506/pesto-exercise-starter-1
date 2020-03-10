db4655133500512329071b97eaae84f145cbdbd0
gauravkr2506 <gauravkr2506@gmail.com>
2020-03-10 13:17:19 +0530

    dat 5 task

---
+++ js-exercises/update-inventory/index.js
@@ -0,0 +1,40 @@
+const describe = (desc, fn) => {
+  console.log(desc);
+  fn();
+};
+
+const test = (msg, fn) => describe("  " + msg, fn);
+
+const matchers = exp => ({
+  toBe: assertion => {
+    if (exp === assertion) {
+      console.log("pass");
+      return true;
+    } else {
+      console.log("fail");
+      return false;
+    }
+  },
+  toEqual: assertion => {
+    if (exp.toString() === assertion.toString()) {
+      console.log("pass");
+      return true;
+    } else {
+      console.log("fail");
+      return false;
+    }
+  }
+});
+
+const expect = exp => matchers(exp);
+
+function adder(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  describe,
+  expect,
+  test,
+  matchers
+};
