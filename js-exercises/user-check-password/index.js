5972dab95ed37de5662217892e5536aa4832d093
gauravkr2506 <gauravkr2506@gmail.com>
2020-03-12 10:03:52 +0530

    Message

---
+++ js-exercises/user-check-password/index.js
@@ -0,0 +1,37 @@
+const describe = (desc, fn) => {
+  console.log(desc);
+  fn();
+};
+
+const test = (msg, fn) => describe("  " + msg, fn);
+
+const matchers = exp => ({
+  toBe: assertion => {
+    if (exp === assertion) {
+      console.log("pass");
+      return true;
+    } else {
+      console.log("fail");
+      return false;
+    }
+  },
+  toEqual: assertion => {
+    if (exp.toString() === assertion.toString()) {
+      console.log("pass");
+      return true;
+    } else {
+      console.log("fail");
+      return false;
+    }
+  }
+});
+
+const expect = exp => matchers(exp);
+
+
+module.exports = {
+  describe,
+  expect,
+  test,
+  matchers
+};
---
+++ js-exercises/user-check-password/user.js
@@ -0,0 +1,21 @@
+// (*)
+// Follow the instructions and fill in the blank sections.
+function User(user_name, password) {
+  // set a username and password property on the user object that is created
+  this.user_name = user_name;
+  this.password = password
+
+}
+
+// create a method on User called `checkPassword`
+// this method should take in a string and compare it to the object's password property
+// return `true` if they match, otherwise return `false`
+User.prototype.checkPassword = function checkPassword(password) {
+  return this.password === password;
+};
+
+
+
+module.exports = {
+  User
+};
