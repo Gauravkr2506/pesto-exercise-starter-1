74fe9421fa9d5efb2021822a69cc0515d8d124cd
gauravkr2506 <gauravkr2506@gmail.com>
2020-03-24 19:48:20 +0530

    day 13 task

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,9 +1,9 @@
-const allPromises = args => {
+const allPromises = async args => {
   if (!Array.isArray(args)) return Promise.resolve();
   let result = [];
   args.forEach(value => {
     if (value instanceof Promise) {
-      value.then(val => result.push(val));
+      result.push(process.binding("util").getPromiseDetails(value)[1]);
     } else {
       result.push(value);
     }
